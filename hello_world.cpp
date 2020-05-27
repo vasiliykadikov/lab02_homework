5ee8b29cdcb09fe6493fb4efd82314eae236183b
vasiliykadikov <vasiliykadikov@inbox.ru>
2020-05-27 16:48:12 +0300

    changed hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 int main () {
-	std::string name;
-	stg::cin >> name;
-	std::cout << "Hello World from " << name;
+	std::string name; 
+	stg::cin >> name; //write your name
+	std::cout << "Hello World from " << name; //cout this text
 }
